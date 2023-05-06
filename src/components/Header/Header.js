355aa34463c4e72786ee0df47ab9b27069694c59
Luka Varshanidze <lukaspc@Lukas-MacBook-Air.local>
2023-05-06 23:33:33 +0400

    first Commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,8 @@ import englandSVG from '../../assets/headerlogo/england.svg'
 import hamburgerLogo from './hamburger.svg'
 
 const Header = () => {
+  const [click, setClick] = useState(false);
+  
   const [dogopedia, setDogopedia] = useState(false);
   return (
     <div className={styles.container}>
@@ -16,7 +18,7 @@ const Header = () => {
           <span>MuskOnMars</span>
         </a>
       </div>
-      <div className={styles.navigation}>
+      <div className={styles.navigation} id='myLinks'>
         <nav>
         <ul className={styles.navigation__leftside}>
           <li><a href='#'>So Home</a></li>
@@ -33,10 +35,28 @@ const Header = () => {
             <span>▾</span>
           </li>
         </ul>
-        <div className={styles.hamburger}>
-          <img src={hamburgerLogo}/>
-        </div>
-        </nav>
+        </nav> 
+      </div> { /* end of .navigation */}
+
+      <div className={styles.hamburger}>
+        <img onClick={() => setClick(prev => !prev)} src={hamburgerLogo}/>
+        {click &&
+          <ul className={styles.navigation__leftside}>
+            <li><a href='#'>So Home</a></li>
+            <li><a href='#aboutMuskOnMars'>What is MOM?</a></li>
+            <li><a href='#howToBuy'>How to buy?</a></li>
+            <li><a href='#'>Very Community</a></li>
+            <li className={styles.dogopedia}>
+              <a className={styles.dogShow} href='#'>So MOM
+                <div className={styles.show}>
+                  <p><a href='#FAQ'>FAQ</a></p>
+                  <p><a href='https://muskonmars.gitbook.io/muskonmars-mom/?fbclid=IwAR2SRIZsZ6IqyENb1XD158eKiqh8cDOTN-T_KrSMxyac4FGeMsBAYMC1jIU'>Whitepaper</a></p>
+                </div>
+              </a>
+              <span>▾</span>
+            </li>
+          </ul>
+        }
       </div>
     </div>
   )
